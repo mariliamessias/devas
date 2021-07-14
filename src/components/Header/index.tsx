060eedd1b00adf6fa24e1a81cff16421d1dbb113
mariliamessias <marilia.oliveira@outlook.com>
2021-07-14 00:19:37 -0300

    create a dropdown menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,38 @@
-import './styles.css';
+import React, { useState } from "react";
+import {useHistory} from 'react-router-dom';
+import styled from "styled-components";
+import Dropdown, { Option } from 'react-dropdown';
 
-export function Header(){
-    return(
-        <div className="header-content">
-            <h1>Devas|</h1>
+import "react-dropdown/style.css";
+import "./styles.css";
 
-            <ul className="header-list">
-                <li>
-                    Trilha Fundamentos
-                </li>
-            </ul>
-        </div>
-    )
+export function Header() {
+    const options = [
+        { value: 'trail-fundamentals', label: "Trilha Fundamentos" }, 
+        { value: '', label: "Trilha FrontEnd"}, 
+        { value: '', label: "Trilha BackEnd"}
+      ];
+      const history = useHistory();
+      const defaultOption = options[0];
+
+      function handleOnSelect(e : Option) {
+        console.log(e.value);
+
+        history.push(`/${e.value}`)
+
+      }
+
+      
+
+  return (
+    <div className="header-content">
+      <h1>Devas|</h1>
+
+      <Dropdown onChange={handleOnSelect} options={options} value={defaultOption} />
+     
+      {/* <ul className="header-list">
+        <li>Trilha Fundamentos</li>
+      </ul> */}
+    </div>
+  );
 }
